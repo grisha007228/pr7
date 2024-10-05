61ce193bb119f9dcfc521da63695c41281d7f5b7
Grisha <spleefxx@gmail.com>
2024-10-05 12:25:20 +0500

    Создал каталог homework4 и добавил туда код для пункта 4

---
+++ homework4/hw4.py
@@ -0,0 +1,9 @@
+print ('Введите переменную a')
+a = int(input())
+print ('Введите переменную b')
+b = int(input())
+print ('Введите переменную c')
+c = int(input())
+
+x = x = a + b * 4 - c
+print('X =', a + b * 4 - c)
